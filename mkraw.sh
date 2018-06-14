d46498f96afdbfe1d6c0baeff26d7b5a1f27cdb7
Allen L. McPherson <github@losalamosal.me>
2018-06-14 15:39:57 -0600

    New data: date

--- mkraw.sh
+++ mkraw.sh
@@ -2,5 +2,9 @@
 #
 # ./mkraw.sh "`date`" "`./node_modules/fast-cli/cli.js`"
 
+git clone git@github.com:losalamosal/jstest.git
+cd jstest
 sed "s+\/\* SED TARGET \*\/+{d: \"$1\", s: \"$2\"},\\
   &+" raw_data.js > raw_data.js.tmp && mv raw_data.js.tmp raw_data.js
+git commit -am "New data: date"
+
