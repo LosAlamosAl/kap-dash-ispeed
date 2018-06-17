64fde98e00391d8e2db586395d5cb6344ebce441
Thine Geezer <github@geezerstyle.com>
2018-06-17 22:03:37 +0000

    Test other commiter

--- raw_data.js
+++ raw_data.js
@@ -1,10 +1,4 @@
 let rawData = [
-  {d: "Thu Jun 14 14:54:49 MDT 2018", s: "85 Mbps"},
-  {d: "Thu Jun 14 14:55:05 MDT 2018", s: "80 Mbps"},
-  {d: "Thu Jun 14 15:32:40 MDT 2018", s: "31 Mbps"},
-  {d: "Thu Jun 14 15:39:45 MDT 2018", s: "92 Mbps"},
-  {d: "Thu Jun 14 15:41:17 MDT 2018", s: "33 Mbps"},
-  {d: "Sun Jun 17 21:40:50 UTC 2018", s: "71 Mbps"},
   {d: "Sun Jun 17 22:00:01 UTC 2018", s: "78 Mbps"},
   /* SED TARGET */
 ];
