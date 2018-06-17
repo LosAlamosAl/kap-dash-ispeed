f272904f7bb15a439abfe9571777a25840cdefa5
Allen McPherson <al@losalamosal.me>
2018-06-17 21:38:53 +0000

    New data: date

--- mkraw.sh
+++ mkraw.sh
@@ -2,12 +2,12 @@
 #
 # ./mkraw.sh "`date`" "`./node_modules/fast-cli/cli.js`"
 DATE=`date`
-FAST-CLI=~/fast-cli/cli.js
+FASTCLI=~/fast-cli/cli.js
 cd ~/jstest
 git pull
 # cd down into internet speed page
 # git clone git@losalamosal.github.com:losalamosal/jstest.git
-sed "s+\/\* SED TARGET \*\/+{d: \"$DATE\", s: \"$FAST-CLI\"},\\
+sed "s+\/\* SED TARGET \*\/+{d: \"$DATE\", s: \"$FASTCLI\"},\\
   &+" raw_data.js > raw_data.js.tmp && mv raw_data.js.tmp raw_data.js
 git commit -am "New data: date"
 git push
