2505f4aac3f7a0f9ad5ee7b48f2c5df6c2ef9f1d
Allen McPherson <al@losalamosal.me>
2018-06-17 21:37:08 +0000

    New data: date

--- raw_data.js
+++ raw_data.js
@@ -4,5 +4,6 @@ let rawData = [
   {d: "Thu Jun 14 15:32:40 MDT 2018", s: "31 Mbps"},
   {d: "Thu Jun 14 15:39:45 MDT 2018", s: "92 Mbps"},
   {d: "Thu Jun 14 15:41:17 MDT 2018", s: "33 Mbps"},
+  {d: "Sun Jun 17 21:37:07 UTC 2018", s: "-CLI"},
   /* SED TARGET */
 ];
