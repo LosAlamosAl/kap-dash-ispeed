b3b0ab5bfc57df8be170968bef6280f35e9dc992
Allen L. McPherson <al@losalamosal.me>
2018-11-27 15:03:13 -0700

    Testing new date range for heatmap

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ Highcharts.chart('speedmap', {
 
   xAxis: {
     type: 'datetime',
-    min: Date.UTC(2018, 0, 1),
+    min: Date.UTC(2018, 7, 1),
     max: Date.UTC(2018, 11, 31, 23, 59, 59),
     labels: {
       align: 'left',
