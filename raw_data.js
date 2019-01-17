6856de863ed0b1d9dc6627591b7ffc8ffea8e210
Don't Pad Commit History <bogus@email>
2019-01-17 15:00:56 +0000

    New data: Thu Jan 17 15:00:54 UTC 2019

--- raw_data.js
+++ raw_data.js
@@ -5127,5 +5127,6 @@ let rawData = [
   {d: "Thu Jan 17 12:00:45 UTC 2019", s: "220 Mbps"},
   {d: "Thu Jan 17 13:00:48 UTC 2019", s: "71 Mbps"},
   {d: "Thu Jan 17 14:00:42 UTC 2019", s: "63 Mbps"},
+  {d: "Thu Jan 17 15:00:54 UTC 2019", s: "77 Mbps"},
   /* SED TARGET */
 ];
